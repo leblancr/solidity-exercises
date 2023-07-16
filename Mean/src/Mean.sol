b7823d202ffae64a81be1daa0d54d6bd5fc82020
rich <rkba2@outlook.com>
2023-07-16 19:50:32 -0400

    finished Mean

--- Mean/src/Mean.sol
+++ Mean/src/Mean.sol
@@ -5,7 +5,15 @@ contract Mean {
     /**
      * The goal of this exercise is to return the mean of the numbers in "arr"
      */
-    function mean(uint256[] calldata arr) public view returns (uint256) {
-        // your code here
+    function mean(uint256[] calldata arr) public pure returns (uint256) {
+        //
+        uint sum = 0;
+        for (uint i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        // Calculate the mean (average)
+        uint mean_of_arr = sum / arr.length;
+        return mean_of_arr;
     }
 }
