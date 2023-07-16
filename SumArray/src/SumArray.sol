45e84c1ce38c86f4c411d226d62a3eb80a1dea25
rich <rkba2@outlook.com>
2023-07-16 16:39:05 -0400

    added license, pragma

--- SumArray/src/SumArray.sol
+++ SumArray/src/SumArray.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
 contract SumArray {
     function sumArray(uint256[] calldata arr) public pure returns (uint256) {
         // your code here
