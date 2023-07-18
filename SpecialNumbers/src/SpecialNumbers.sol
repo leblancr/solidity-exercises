30bfbc09f6aa77248a395a40d4cb15485fb59eda
rich <rkba2@outlook.com>
2023-07-18 17:31:31 -0400

    finished SpecialNumbers

--- SpecialNumbers/src/SpecialNumbers.sol
+++ SpecialNumbers/src/SpecialNumbers.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
 contract SpecialNumbers {
-    mapping(uint256 => bool) public isSpecial;
+    mapping(uint256 => bool) public isSpecial;  // map is dictionary
 
     /**
      * The goal is to use mappings and store if a number is special or not (using booleans)
@@ -10,16 +13,19 @@ contract SpecialNumbers {
 
     /// make @param n special
     function makeNumberSpecial(uint256 n) public {
-        // your code here
+        // is special
+        isSpecial[n] = true;
     }
 
     /// make @param n not special
     function makeNumberNotSpecial(uint256 n) public {
-        // your code here
+        // is not special
+        isSpecial[n] = false;
     }
 
     /// return if a number @param n is special or not
     function isNumberSpecial(uint256 n) public view returns (bool) {
-        // your code here
+        // return
+        return isSpecial[n];
     }
 }
