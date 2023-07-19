4c34269712e8adec681f7098e075803547688ec1
rich <rkba2@outlook.com>
2023-07-19 08:52:52 -0400

    finished Decoder

--- Decoder/src/Decoder.sol
+++ Decoder/src/Decoder.sol
@@ -8,8 +8,8 @@ contract Decoder {
     */
     bytes public encoded;
 
-    function decodeData(
-        bytes memory _data
-    ) public pure returns (string memory, uint256) {}
+    function decodeData(bytes memory _data) public pure returns (string memory, uint256) {
+        (string  memory x, uint256 y) = abi.decode(_data, (string, uint256));
+        return(x,y);
+    }
 }
-
