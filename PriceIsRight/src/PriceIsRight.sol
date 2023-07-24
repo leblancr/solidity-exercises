1ec1b5b1bcfffdb0420a5f66974851b91aa93abd
rich <rkba2@outlook.com>
2023-07-24 15:24:19 -0400

    Finished PriceIsRight

--- PriceIsRight/src/PriceIsRight.sol
+++ PriceIsRight/src/PriceIsRight.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 contract PriceIsRight {
     /// @dev make this function revert unless exactly 1 ether is sent as value in the function call
     function buy() public payable {
-        /// your code here
+        if (msg.value != 1) {
+            revert("Input must be one.");
+        }
     }
 }
